0adabf200cc3f81f69a15b895f913989e61c18f9
amit-fueled <amit@fueled.com>
2017-01-25 18:53:54 +0530

    feat(reactive-loader): show and hide loader reactively

--- Reactive/ViewController.swift
+++ Reactive/ViewController.swift
@@ -11,19 +11,77 @@ import ReactiveCocoa
 import ReactiveSwift
 import enum Result.NoError
 
+protocol Loader {
+	var indictaorView : UIActivityIndicatorView? { get }
+	func showLoader()
+	func hideLoader()
+}
+
+extension Loader where Self: UIView {
+	
+	var indictaorView : UIActivityIndicatorView? {
+		get { return subviews.filter { $0 is UIActivityIndicatorView }.first as? UIActivityIndicatorView }
+	}
+	
+	func showLoader() {
+		let indictaorView = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+		indictaorView.center = CGPoint(x: bounds.width/2, y: bounds.height/2)
+		let overlay = UIView(frame: bounds)
+		overlay.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+		addSubview(overlay)
+		addSubview(indictaorView)
+		indictaorView.startAnimating()
+	}
+	
+	func hideLoader(){
+		guard let indictaorView = indictaorView else { return }
+		indictaorView.stopAnimating()
+		indictaorView.removeFromSuperview()
+	}
+}
+
+extension UIButton: Loader{}
+
 class ViewController: UIViewController {
 
+	@IBOutlet weak var button: UIButton!
 	@IBOutlet weak var textField: UITextField!
 	@IBOutlet weak var label: UILabel!
+	let buttonSignal = Signal<Bool, NoError>.pipe()
+
+	let isApiCallExcecuting = MutableProperty<Bool>(false)
+	
+	@IBAction func didTapButton(_ sender: Any) {
+		isApiCallExcecuting.value = true
+		makeAPICall {
+			self.isApiCallExcecuting.value = false
+		}
+	}
+
+	func makeAPICall(_ handler: @escaping () -> Void) {
+		let delay = DispatchTime.now() + 5
+		DispatchQueue.main.asyncAfter(deadline: delay) {
+			handler()
+		}
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		let textSignal = textField.reactive.continuousTextValues
-		let isHiddenSignal = textSignal.skipNil().map { ($0.characters.count) < 6 }
-		
-		label.reactive.isHidden <~ isHiddenSignal
-		label.reactive.text <~ textSignal
+		button.reactive.shouldLoad <~ isApiCallExcecuting.producer
 	}
 }
+extension Reactive where Base: UIButton {
+		internal var shouldLoad: BindingTarget<Bool> {
+		return makeBindingTarget {
+			if let b = self.base as? Loader {
+				if $1 {
+					b.showLoader()
+				} else {
+					b.hideLoader()
+				}
+			}
+		}
+	}
+}
+
 
